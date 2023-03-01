2dfcadb3c9d7c7ac945aef02988f0867e43e821f
David <61595414+dhedderich@users.noreply.github.com>
2023-03-01 14:03:25 +0000

    Added all unit tests except one modified:   churn_script_logging_and_tests.py modified:   requirements.txt

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import numpy as np
 from churn_library import *
 
 logging.basicConfig(
@@ -21,18 +22,19 @@ class TestClass():
 		test data import - this example is completed for you to assist with the other test functions
 		'''		
 
+		# Test if the correct file exists in the correct directory
 		try:
-			df = self.predictor.import_data()
-			logging.info("Testing import_data: SUCCESS")
+			self.df = self.predictor.import_data()
+			logging.info("INFO: Testing import_data: SUCCESS")
 		except FileNotFoundError as err:
-			logging.error("Testing import_eda: The file wasn't found")
+			logging.error("ERROR: Testing import_data: The file wasn't found")
 			raise err
 
 		try:
 			assert df.shape[0] > 0
 			assert df.shape[1] > 0
 		except AssertionError as err:
-			logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+			logging.error("ERROR: Testing import_data: The file doesn't appear to have rows and columns")
 			raise err
 
 
@@ -41,11 +43,53 @@ class TestClass():
 		test perform eda function
 		'''
 
+		try:
+			file_path = './images/eda'
+			target_directory = os.path.dirname(file_path)
+			assert os.path.exist(target_directory) = True
+			logging.info("INFO: Testing perform_eda: SUCCESS")
+		except FileNotFoundError as err:
+			logging.error("ERROR: Testing perform_eda: The target directory wasn't found")
+			raise err
+	
+		try:
+			cat_columns = self.df.select_dtypes(include='object').columns
+			num_columns = self.df.select_dtypes(
+				include=['int', 'float']).columns
+			
+			for column in cat_columns:
+				image_file_path = f'images/eda/{column}_cat_univariate.png'
+				current_column = column
+				assert os.path.exists(image_file_path) = True
+				logging.info(f"INFO: Testing perform eda - EDA result for column {column} saved correctly: SUCCESS")
+
+			for column in num_columns:
+				image_file_path = f'images/eda/{column}_num_univariate.png'
+				current_column = column
+				assert os.path.exists(image_file_path) = True
+				logging.info(f"INFO: Testing perform eda - EDA result for column {column} saved correctly: SUCCESS")
+
+		except FileNotFoundError as err:
+				logging.error(f"ERROR: Testing perform_eda: The EDA result for column {current_column} wasn't found")
+				raise err
+
 
 	def test_encoder_helper(self):
 		'''
 		test encoder helper
 		'''
+		
+		# Check if all mean encodings worked accordingly and created an entry between 0 and 1 for a binary classification problem
+		try:
+			for column in cat_columns:
+				unique_values = self.df[column].unique()
+				within_zero_and_one = np.logical_and(unique_values >= 0, unique_values <= 1)
+				assert np.any(np.logical_not(between_zero_and_one)) = False
+			logging.info("INFO: Testing encoder_helper: SUCCESS")
+		
+		except AssertionError as err:
+			logging.error("ERROR: Testing encoder_helper: The mean encoder did not work as intended due to values outside of [0,1]")
+			raise err
 
 
 	def test_perform_feature_engineering(self):
@@ -58,7 +102,27 @@ class TestClass():
 		'''
 		test train_models
 		'''
+		# Check if directory ./models exists
+		try:
+			file_path = './models'
+			target_directory = os.path.dirname(file_path)
+			assert os.path.exist(target_directory) = True
+			logging.info("INFO: Testing train_models - Target directory exists: SUCCESS")
+		except FileNotFoundError as err:
+			logging.error("ERROR: Testing train_models: The target directory wasn't found")
+			raise err
 
+		# Check if models were created successfully in ./models/
+		try:
+			logreg_file_path = './models/logistic_model.pkl'
+			rf_file_path = './models/rfc_model.pkl'
+			assert os.path.exists(logreg_file_path) = True
+			assert os.path.exists(rf_file_path) = True
+			logging.info('INFO: Testing train_models - Models were created successfully: SUCCESS')
+		
+		except FileNotFoundError as err:
+			logging.error(f"ERROR: Testing train_models: One of the models wasn't found and thereby potentially not created successfully")
+			raise err
 
 if __name__ == "__main__":
 	tester = TestClass()
